e474bbe3d8c40342154d4d4627eda886c74b92ac
Vibin Daniel <vibind@drivevariant.com>
2021-02-01 12:38:44 -0500

    [INIT] fix file not found
    [INIT] test
    
    [INIT] fix action
    
    [INIT] fix action path
    
    [INIT] more permissions
    
    [INIT] more permissions
    
    [INIT] fix dir
    
    [INIT] find the gitversion folder
    
    [INIT] fix error
    
    [INIT] remove sudo from action
    
    [INIT] fix github_env
    
    [INIT] fix env
    
    [INIT] try 2
    
    [INIT] fix aws creds
    
    [INIT] add sonar project key
    
    [INIT] fix variable
    
    [INIT] add to github env
    
    [INIT] set env variable
    
    [INIT] use write-host
    
    [INIT] use powershell convention
    
    [INIT] l
    
    [INIT] a
    
    [INIT] a
    
    [INIT] a
    
    [INIT] cleanup
    
    [INIT] save token file to variable
    
    [INIT] remove mask
    
    [INIT] echo file
    
    [INIT] get app token
    
    [INIT] debug
    
    [INIT] add github repository
    
    [INIT] remove debug
    
    [INIT] try with pat
    
    [INIT] remove secret
    
    [INIT] remove github token

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,15 @@
 set -e
 
 ## Reference: https://github.com/GitTools/actions/
-curl -sL https://raw.githubusercontent.com/GitTools/actions/main/dist/github/gitversion/execute/bundle.js -o ../_temp/gitversion-execute.js
-node ../_temp/gitversion-execute.js
-rm ../_temp/gitversion-execute.js
+curl -sL https://raw.githubusercontent.com/GitTools/actions/main/dist/github/gitversion/execute/bundle.js -o gitversion-execute.js
+node gitversion-execute.js
+rm gitversion-execute.js
 
 echo "::add-mask::$SONAR_TOKEN"
 echo "SONAR_TOKEN=$SONAR_TOKEN" >> $GITHUB_ENV
 echo "SONAR_ORG=$SONAR_ORG" >> $GITHUB_ENV
-echo "AWS_ROLE_ARN=$AWS_WEB_IDENTITY_TOKEN_FILE" >> $GITHUB_ENV
-echo "::add-mask::$AWS_WEB_IDENTITY_TOKEN_FILE"
-echo "AWS_ROLE_ARN=$AWS_WEB_IDENTITY_TOKEN_FILE" >> $GITHUB_ENV
 
-versionNumber="${GitVersion_NuGetVersion}.${GITHUB_RUN_NUMBER}"
-Write-Host "Setting environment variable IMAGE_VERSION to: $versionNumber"
-echo "IMAGE_VERSION=$versionNumber" >> ${env:GITHUB_ENV}
-echo "::set-output name=image-version::$versionNumber"
+echo "AWS_ROLE_ARN=$AWS_ROLE_ARN" >> $GITHUB_ENV
+AWS_WEB_IDENTITY_TOKEN="$(cat $AWS_WEB_IDENTITY_TOKEN_FILE)"
+echo "::add-mask::$AWS_WEB_IDENTITY_TOKEN"
+echo "AWS_WEB_IDENTITY_TOKEN=$AWS_WEB_IDENTITY_TOKEN" >> $GITHUB_ENV
